0f20b6538e8de4057381df71255553efd943c792
chandrasekhara <chandrasekhara@proarch.com>
2020-04-10 09:37:45 +0530

    changed nodejs

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -33,6 +33,48 @@ xlsxFile('./src/assets/spreedsheets/SampleData.xlsx', { getSheets: true }).then(
     })
   })
 })
+
+var indiaCovidData = {
+  predictionData: [],
+  latestData: [],
+  actualDateWiseData: [],
+  hospitalTestData: [],
+  stateWiseData: [],
+  indiaData: []
+}
+
+indiaCovidData.predictionData = xlsxFile('./src/assets/spreedsheets/India_Prediction.xlsx').then((rows) => {
+  return rows;
+});
+xlsxFile('./src/assets/spreedsheets/India_Latest.xlsx').then((rows) => {
+  indiaCovidData.latestData.push(rows);
+});
+xlsxFile('./src/assets/spreedsheets/Inida_Actuals_Daywise.xlsx').then((rows) => {
+  indiaCovidData.actualDateWiseData.push(rows);
+});
+xlsxFile('./src/assets/spreedsheets/India _hospital_testing_data.xlsx').then((rows) => {
+  indiaCovidData.hospitalTestData.push(rows);
+});
+xlsxFile('./src/assets/spreedsheets/State_Wise_Actuals.xlsx').then((rows) => {
+  indiaCovidData.stateWiseData.push(rows);
+});
+xlsxFile('./src/assets/spreedsheets/India.xlsx').then((rows) => {
+  indiaCovidData.indiaData.push(rows);
+});
+
+
+app.get('/india-data', (req, res) => {
+ 
+  console.log(indiaCovidData);
+
+});
+
+
+
+
+
+
+
 app.get('/data', (req, res) => {
   res.send(data);
 });
